dcd2959f137336860d8312fe5e62e84d8ff7c7b4
Maxi <herreramaxi@gmail.com>
2020-09-07 23:56:51 +0100

    fixing input parameters on msbuild task and generating a ts script from consoleApp assembly

--- BackendExporter/Program.cs
+++ BackendExporter/Program.cs
@@ -1,18 +1,17 @@
-﻿using EnumGenerator;
-using System;
+﻿using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 
 namespace BackendExporter
 {
-   public class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             //TODO: it is pending to handle parameters and load assembly
-            var projectPath = args[1];
-            var assemblyFile = args[0];
+            var projectPath = args[0];
+            var assemblyFile = args[1];
             Console.WriteLine($"projectPath: {projectPath}");
             Console.WriteLine($"assemblyFile: {assemblyFile}");
 
@@ -20,12 +19,13 @@ namespace BackendExporter
             Console.WriteLine($"assemblyPath: {assemblyPath}");
 
             //var configFile = ConfigurationFileFinder.Find(projectPath);
-            //var assembly = Assembly.LoadFile(assemblyPath);
-            //var enumTypes = assembly.GetTypes()
-            //                         .Where(t => t.GetCustomAttribute<EnumTBEAttribute>() != null)
-            //                         .ToArray();
-            //var script = new EnumGenerator.EnumGenerator(enumTypes).TransformText();
-            Console.WriteLine("Hello World!");
+            var assembly = Assembly.LoadFile(assemblyPath);
+            var enumTypes = assembly.GetTypes()
+                                     .Where(t => t.GetCustomAttribute<EnumTBEAttribute>() != null)
+                                     .ToArray();
+            var script = new EnumGenerator.EnumGenerator(enumTypes).TransformText();
+            Console.WriteLine("script:");
+            Console.WriteLine(script);
         }
     }
 }
